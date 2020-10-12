44a5765ccf3b1e9e0936d8dc1038fc2a803c6733
urbeingwathced8 <tupikina_natasha@mail.ru>
2020-10-12 21:45:30 +0300

    bug fix and req.txt update

--- namenode/namenode.py
+++ namenode/namenode.py
@@ -14,12 +14,14 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("ip")
 parser.add_argument("port")
+parser.add_argument("client_port")
+
 args = parser.parse_args()
 
 IP = args.ip
 PORT = int(args.port)
-CLIENT_IP = '127.0.0.1'
-CLIENT_PORT = 8800
+CLIENT_IP = args.ip
+CLIENT_PORT = int(args.client_port)
 BUFFER_SIZE = 1024
 datanodes_number = 2
 sockets = {}
@@ -34,16 +36,18 @@ sock.bind((IP, PORT))
 sock.listen(5)
 print('Listening..')
 
+
 def handle_conn():
     while True:
-            node_conn, addr = sock.accept()
-            node_port =  node_conn.recvfrom(1024)
-            print("Node at {}: connected". format(addr))
-            index = addr[0] + ":" + node_port[0].decode()
-            datanodes.append(index)
-            sockets[index] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sockets[index].connect((addr[0], int(node_port[0].decode())))
-            conn[index] =  node_conn
+        node_conn, addr = sock.accept()
+        node_port = node_conn.recvfrom(1024)
+        print("Node at {}: connected".format(addr))
+        index = addr[0] + ":" + node_port[0].decode()
+        datanodes.append(index)
+        sockets[index] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockets[index].connect((addr[0], int(node_port[0].decode())))
+        conn[index] = node_conn
+
 
 def check_nodes_activity():
     while True:
@@ -51,15 +55,19 @@ def check_nodes_activity():
             node_addr = datanode.split(":")
             response = subprocess.getstatusoutput("ping -c 1 " + node_addr[0])
             if response[0] == 1:
-                print("Node at {} stopped working, starting backup process". format(node_addr))
+                print("Node at {} stopped working, starting backup process".format(node_addr))
                 backup(datanode)
         time.sleep(5)
+
 
 def initialize_storage():
     for i in sockets.values():
         i.send(bytes("init", "utf-8"))
     make_query("DROP TABLE IF EXISTS filesdb;", is_return=False)
-    make_query("CREATE TABLE filesdb (filename Text, datanode1 TEXT, datanode2 TEXT, dir TEXT, is_dir BOOLEAN, size TEXT);", False)
+    make_query(
+        "CREATE TABLE filesdb (filename Text, datanode1 TEXT, datanode2 TEXT, dir TEXT, is_dir BOOLEAN, size TEXT);",
+        False)
+
 
 def is_exists(filename):
     if current_dir != '/':
@@ -68,23 +76,28 @@ def is_exists(filename):
         path = current_dir + filename
     is_exist_path(path)
 
+
 def is_exist_path(filepath):
-    length = len(make_query("SELECT * From filesdb where filename='{}'". format(filepath), True))
+    length = len(make_query("SELECT * From filesdb where filename='{}'".format(filepath), True))
     if length == 0:
         return False
     return True
 
+
 def get_file_ips(filepath):
-    requested_file = make_query("SELECT * FROM filesdb WHERE filename='{}'". format(filepath), True)
+    requested_file = make_query("SELECT * FROM filesdb WHERE filename='{}'".format(filepath), True)
     return requested_file[0][1], requested_file[0][2]
+
 
 def get_ips():
     count = random.sample(datanodes, 2)
     return count
 
+
 def mkdir(addr, path):
     sockets[addr].send(bytes("makedir " + storage + path, "utf-8"))
     client_conn.send("OK".encode())
+
 
 def mkdir_current(new_path):
     if is_exists(new_path) != True:
@@ -92,25 +105,28 @@ def mkdir_current(new_path):
             path = current_dir + "/" + new_path
         else:
             path = current_dir + new_path
-        make_query("Insert into filesdb(filename, datanode1, datanode2, dir, is_dir, size) VALUES ('{0}','{1}','{2}','{3}', {4}, '-')".format(path, "_", "_", current_dir, True), False)
+        make_query(
+            "Insert into filesdb(filename, datanode1, datanode2, dir, is_dir, size) VALUES ('{0}','{1}','{2}','{3}', {4}, '-')".format(
+                path, "_", "_", current_dir, True), False)
         for i in sockets.values():
             i.send(bytes("makedir " + storage + path, "utf-8"))
         client_conn.send("OK".encode())
     else:
-        client_conn.send(("Already Exists:'{}'". format(new_path)).encode())
+        client_conn.send(("Already Exists:'{}'".format(new_path)).encode())
+
 
 def create_file(filename):
     if is_exists(filename) != True:
         ips = get_ips()
         if current_dir != "/":
-            sockets[ips[0]].send(bytes("create " + storage+current_dir + "/" + filename, "utf-8"))
-            sockets[ips[1]].send(bytes("create " + storage+current_dir + "/" + filename, "utf-8"))
+            sockets[ips[0]].send(bytes("create " + storage + current_dir + "/" + filename, "utf-8"))
+            sockets[ips[1]].send(bytes("create " + storage + current_dir + "/" + filename, "utf-8"))
             make_query(
                 "Insert into filesdb(filename, datanode1, datanode2, dir, is_dir, size) VALUES ('{0}','{1}','{2}','{3}', {4}, '0')"
                     .format(current_dir + "/" + filename, ips[0], ips[1], current_dir, False), False)
         else:
-            sockets[ips[0]].send(bytes("create " + storage+current_dir + filename, "utf-8"))
-            sockets[ips[1]].send(bytes("create " + storage+current_dir + filename, "utf-8"))
+            sockets[ips[0]].send(bytes("create " + storage + current_dir + filename, "utf-8"))
+            sockets[ips[1]].send(bytes("create " + storage + current_dir + filename, "utf-8"))
             make_query(
                 "Insert into filesdb(filename, datanode1, datanode2, dir, is_dir, size) VALUES ('{0}','{1}','{2}','{3}', {4}, '0')"
                     .format(current_dir + filename, ips[0], ips[1], current_dir, False), False)
@@ -120,7 +136,8 @@ def create_file(filename):
             path = current_dir + "/" + filename
         else:
             path = current_dir + filename
-        client_conn.send(("Already exists:'{}'". format(path)).encode())
+        client_conn.send(("Already exists:'{}'".format(path)).encode())
+
 
 def read(filename):
     if is_exists(filename):
@@ -133,7 +150,7 @@ def read(filename):
         sock = sockets[ips[0]]
         node_conn = conn[ips[0]]
         sock.send(bytes("read " + storage + path, "utf-8"))
-        pathlib.Path('/received_files'+path[:path.rfind("/")+1]).mkdir(parents=True, exist_ok=True)
+        pathlib.Path('/received_files' + path[:path.rfind("/") + 1]).mkdir(parents=True, exist_ok=True)
         with open('/received_files' + path, 'wb') as handle:
             if node_conn.recv(1024) == b'1':
                 s = node_conn.recv(1024)
@@ -153,23 +170,27 @@ def read(filename):
             path = current_dir + "/" + filename
         else:
             path = current_dir + filename
-        client_conn.send(("Not found:'{}'". format(path)).encode())
+        client_conn.send(("Not found:'{}'".format(path)).encode())
+
 
 def write(path, fs_path):
     ips = get_ips()
     if len(fs_path[:fs_path.rfind('/')]) == 0:
-        make_query("INSERT INTO filesdb(filename, datanode1, datanode, dir,is_dir, size) VALUES ('{}','{}','{}', '{}',{}, '{}')".
-               format(fs_path, ips[0], ips[1], '/',False, os.path.getsize(path)), False)
+        make_query(
+            "INSERT INTO filesdb(filename, datanode1, datanode, dir,is_dir, size) VALUES ('{}','{}','{}', '{}',{}, '{}')".
+            format(fs_path, ips[0], ips[1], '/', False, os.path.getsize(path)), False)
     else:
-        make_query("INSERT INTO filesdb(filename, datanode1, datanode, dir,is_dir, size) VALUES ('{}','{}','{}', '{}',{}, '{}')".
-               format(fs_path, ips[0], ips[1], fs_path[:fs_path.rfind('/')],False, os.path.getsize(path)), False)
+        make_query(
+            "INSERT INTO filesdb(filename, datanode1, datanode, dir,is_dir, size) VALUES ('{}','{}','{}', '{}',{}, '{}')".
+            format(fs_path, ips[0], ips[1], fs_path[:fs_path.rfind('/')], False, os.path.getsize(path)), False)
     for i in ips:
         send_file(path, fs_path, i)
     client_conn.send("OK".encode())
 
+
 def send_file(path, fs_path, addr):
     sock = sockets[addr]
-    sock.send(bytes("write {}". format(storage + fs_path), "utf-8"))
+    sock.send(bytes("write {}".format(storage + fs_path), "utf-8"))
     time.sleep(2)
     file1 = open(path, 'rb')
     content = file1.read(1024)
@@ -180,6 +201,7 @@ def send_file(path, fs_path, addr):
     time.sleep(2)
     sock.send(b'0')
     file1.close()
+
 
 def delete_dir(dirname):
     if is_exists(dirname):
@@ -209,7 +231,8 @@ def delete_dir(dirname):
             path = current_dir + "/" + dirname
         else:
             path = current_dir + dirname
-        client_conn.send(("Not found:'{}'". format(path)).encode())
+        client_conn.send(("Not found:'{}'".format(path)).encode())
+
 
 def delete_file(filename):
     if is_exists(filename):
@@ -217,13 +240,13 @@ def delete_file(filename):
             path1 = current_dir + "/" + filename
         else:
             path1 = current_dir + filename
-        requested_file = make_query("SELECT * FROM filesdb WHERE filename='{}'". format(path1), True)
+        requested_file = make_query("SELECT * FROM filesdb WHERE filename='{}'".format(path1), True)
         path = storage + path1
         if len(requested_file) == 0:
             print("File doesn't exist")
             return
         else:
-            make_query("DELETE FROM filesdb Where filename='{}'". format(path1), False)
+            make_query("DELETE FROM filesdb Where filename='{}'".format(path1), False)
             sockets[requested_file[0][1]].send(bytes("delete {}".format(path), "utf-8"))
             sockets[requested_file[0][2]].send(bytes("delete {}".format(path), "utf-8"))
         client_conn.send("OK".encode())
@@ -232,7 +255,8 @@ def delete_file(filename):
             path = current_dir + "/" + filename
         else:
             path = current_dir + filename
-        client_conn.send(("Not found:'{}'". format(path)).encode())
+        client_conn.send(("Not found:'{}'".format(path)).encode())
+
 
 def ls():
     requested_files = make_query("SELECT * FROM filesdb WHERE dir = '{0}';".format(current_dir), True)
@@ -242,36 +266,40 @@ def ls():
             response = response + "Directory {}".format(requested_file[0]) + "\n"
         else:
             response = response + "File {}".format(requested_file[0]) + "\n"
+    response = "empty" if response == "" else response
     client_conn.send(response.encode())
+
 
 def cd(path):
     global current_dir
     if path == "/" or is_exists(path):
         print(path)
         if path == "/":
-            current_dir = "/" 
+            current_dir = "/"
         else:
             if current_dir == "/":
-                current_dir = current_dir + path  
+                current_dir = current_dir + path
             else:
                 current_dir = current_dir + "/" + path  # /
         client_conn.send("OK".encode())
     else:
-        client_conn.send(("Not found:'{}'". format(path)).encode())
+        client_conn.send(("Not found:'{}'".format(path)).encode())
+
 
 def cp(src, dest):
-    if is_exist_path(src)==True and is_exist_path(dest)==False:
+    if is_exist_path(src) == True and is_exist_path(dest) == False:
         ips = get_file_ips(src)
         make_query(
             "Insert into filesdb(filename, datanode1, datanode2, dir, is_dir) VALUES ('{0}','{1}','{2}','{3}', {4})"
                 .format(dest, ips[0], ips[1], dest[:dest.rfind('/')], False), False)
-        sockets[ips[0]].send(bytes("copy {} {}".format(storage+src, storage+dest), "utf-8"))
-        sockets[ips[1]].send(bytes("copy {} {}".format(storage+src, storage+dest), "utf-8"))
+        sockets[ips[0]].send(bytes("copy {} {}".format(storage + src, storage + dest), "utf-8"))
+        sockets[ips[1]].send(bytes("copy {} {}".format(storage + src, storage + dest), "utf-8"))
         client_conn.send("OK".encode())
-    elif is_exist_path(src)==False:
-        client_conn.send(("Not found:'{}'". format(src)).encode())
+    elif is_exist_path(src) == False:
+        client_conn.send(("Not found:'{}'".format(src)).encode())
     else:
-        client_conn.send(("Already Exists:'{}'". format(dest)).encode())
+        client_conn.send(("Already Exists:'{}'".format(dest)).encode())
+
 
 def mv(src, dest):
     if is_exist_path(src):
@@ -281,11 +309,12 @@ def mv(src, dest):
                 .format(dest, ips[0], ips[1], dest[:dest.rfind('/')], False), False)
         make_query("DELETE FROM filesdb Where filename='{}'".format(src), False)
         print(ips)
-        sockets[ips[0]].send(bytes("move " + storage+src + " " + storage+dest, "utf-8"))
-        sockets[ips[1]].send(bytes("move " + storage+src + " " + storage+dest, "utf-8"))
+        sockets[ips[0]].send(bytes("move " + storage + src + " " + storage + dest, "utf-8"))
+        sockets[ips[1]].send(bytes("move " + storage + src + " " + storage + dest, "utf-8"))
         client_conn.send("OK".encode())
     else:
-        client_conn.send(("Not found:'{}'". format(src)).encode())
+        client_conn.send(("Not found:'{}'".format(src)).encode())
+
 
 def make_query(query, is_return):
     conn = psycopg2.connect(dbname='postgres', user='postgres', password='postgres', host='localhost', port="5432")
@@ -301,22 +330,24 @@ def make_query(query, is_return):
 
 
 def backup(addr):
-    backup_files = make_query("SELECT * FROM filesdb Where (datanode1='{}' OR datanode2='{}') AND is_dir=FALSE;". format(addr, addr), True)
-    backup_dir = '/backup_{}'. format(addr)
+    backup_files = make_query(
+        "SELECT * FROM filesdb Where (datanode1='{}' OR datanode2='{}') AND is_dir=FALSE;".format(addr, addr), True)
+    backup_dir = '/backup_{}'.format(addr)
     for backup_file in backup_files:
         if backup_file[1] != addr:
             backup_read(backup_file[0], backup_file[1], backup_dir)  # TODO
         else:
             backup_read(backup_file[0], backup_file[2], backup_dir)
         backup_write(backup_file, backup_file[0], addr, backup_dir)
-    print("Finished backup of node at {}". format(addr))
+    print("Finished backup of node at {}".format(addr))
     datanodes.remove(addr)
+
 
 def backup_read(filename, addr, backup_dir):
     sock = sockets[addr]
     node_conn = conn[addr]
     sock.send(bytes("read " + storage + filename, "utf-8"))
-    pathlib.Path(backup_dir+filename[:filename.rfind("/")+1]).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(backup_dir + filename[:filename.rfind("/") + 1]).mkdir(parents=True, exist_ok=True)
     with open(backup_dir + filename, 'wb') as handle:
         s = node_conn.recv(1024)
         if s == b'1':
@@ -332,6 +363,7 @@ def backup_read(filename, addr, backup_dir):
         else:
             handle.close()
 
+
 def backup_write(fileinf, filename, addr, backup_dir):
     if len(datanodes) <= datanodes_number:
         print("Cannot create replica. Could not find some datanodes")
@@ -340,10 +372,10 @@ def backup_write(fileinf, filename, addr, backup_dir):
             if datanode != fileinf[1] and datanode != fileinf[2]:
                 send_file(backup_dir + filename, filename, datanode)
                 if addr == fileinf[1]:
-                    make_query("UPDATE filesdb set datanode1='{}'". format(datanode), False)
+                    make_query("UPDATE filesdb set datanode1='{}'".format(datanode), False)
                 else:
-                    make_query("UPDATE filesdb set datanode='{}'". format(datanode), False)
-    
+                    make_query("UPDATE filesdb set datanode='{}'".format(datanode), False)
+
 
 def close():
     for i in datanodes:
@@ -355,7 +387,9 @@ def close():
 
 
 if __name__ == "__main__":
-    make_query("CREATE TABLE IF NOT EXISTS filesdb (filename Text, datanode1 TEXT, datanode2 TEXT, dir TEXT, is_dir BOOLEAN, size TEXT);", False)
+    make_query(
+        "CREATE TABLE IF NOT EXISTS filesdb (filename Text, datanode1 TEXT, datanode2 TEXT, dir TEXT, is_dir BOOLEAN, size TEXT);",
+        False)
 
     thread1 = Thread(target=handle_conn)
     thread2 = Thread(target=check_nodes_activity)
@@ -418,5 +452,3 @@ if __name__ == "__main__":
         except:
             client_conn.send("Something went wrong".encode())
             continue
-
-
